7f5e145a59613d1f5737f6aef9f8b10fe68a6ff2
kentrino <kentrino@h-kento.jp>
2018-03-22 21:55:42 +0900

    fix fixture. add is_in_shelf to books

--- db/fixtures/books.rb
+++ db/fixtures/books.rb
@@ -1,15 +1,16 @@
 yaml = "#{Rails.root}/db/fixtures/books.yml"
 books = YAML.load_file(yaml)
 
-def create_book(id: nil, title: nil, author: nil)
+def create_book(id: nil, title: nil, author: nil, is_in_shelf: nil)
   Book.seed do |s|
     s.id = id
     s.title = title
     s.author = author
-    s.is_in_shelf = false
+    # 三項演算子
+    s.is_in_shelf = is_in_shelf.nil? ? false : is_in_shelf
   end
 end
 
 books.map(&:symbolize_keys).each.with_index do |book, i|
-  create_book(id: i + 1, title: book[:title], author: book[:author])
+  create_book(id: i + 1, title: book[:title], author: book[:author], is_in_shelf: book[:is_in_shelf])
 end
