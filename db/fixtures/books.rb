c25da6ae10b8124999ffddb5ee4c3b0c26358d82
kentrino <kentrino@h-kento.jp>
2018-03-22 21:22:28 +0900

    create book seeds

---
+++ db/fixtures/books.rb
@@ -0,0 +1,14 @@
+yaml = "#{Rails.root}/db/fixtures/books.yml"
+books = YAML.load_file(yaml)
+
+def create_book(id: nil, title: nil, author: nil)
+  Book.seed do |s|
+    s.id = id
+    s.title = title
+    s.author = author
+  end
+end
+
+books.map(&:symbolize_keys).each.with_index do |book, i|
+  create_book(id: i + 1, title: book[:title], author: book[:author])
+end
