2a0a95673101f57fb55057bf9cc2fe95cd1e578c
kentrino <kentrino@h-kento.jp>
2018-03-22 21:56:09 +0900

    :sparkles: implement BookShelf

---
+++ app/models/book_shelf.rb
@@ -0,0 +1,11 @@
+class BookShelf
+  def initialize(book_list: nil)
+    @book_list = book_list
+  end
+
+  def get
+    # .filter { |book| book.is_in_shelf }
+    # https://qiita.com/kenzan100/items/6344766167299b9f7e91
+    @book_list.get.to_a.select(&:is_in_shelf)
+  end
+end
