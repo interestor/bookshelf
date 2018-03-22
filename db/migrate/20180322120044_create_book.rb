ff9ce00f462cb811570c37bee85a45e82e049ec3
kentrino <kentrino@h-kento.jp>
2018-03-22 21:24:57 +0900

    change migration. add is_in_shelf to books

--- db/migrate/20180322120044_create_book.rb
+++ db/migrate/20180322120044_create_book.rb
@@ -3,6 +3,7 @@ class CreateBook < ActiveRecord::Migration[5.1]
     create_table :books do |t|
       t.string :title
       t.string :author
+      t.boolean :is_in_shelf
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20180322120044) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
+    t.boolean "is_in_shelf"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
