f32bdeef9dbdec41d68f4b417e2a592ebb2474f5
kentrino <kentrino@h-kento.jp>
2018-03-22 22:06:17 +0900

    (BookShelfController) change to use print method of BookShelf

--- app/controllers/book_shelf_controller.rb
+++ app/controllers/book_shelf_controller.rb
@@ -2,6 +2,6 @@ class BookShelfController < ApplicationController
   def show
     book_list = BookList.new
     book_shelf = BookShelf.new(book_list: book_list)
-    render json: book_shelf.get.to_json
+    render plain: book_shelf.print
   end
 end
