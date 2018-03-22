dfef78c85f57ec41c0d3504d08e35145c77d4867
kentrino <kentrino@h-kento.jp>
2018-03-22 22:05:46 +0900

    add print method to BookShelf

--- app/models/book_shelf.rb
+++ app/models/book_shelf.rb
@@ -3,6 +3,11 @@ class BookShelf
     @book_list = book_list
   end
 
+  def print
+    # 下のprintはbookのprint
+    get.map(&:print).join("<br />")
+  end
+
   def get
     # .filter { |book| book.is_in_shelf }
     # https://qiita.com/kenzan100/items/6344766167299b9f7e91
