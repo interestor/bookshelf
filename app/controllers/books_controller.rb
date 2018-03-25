7a6a5ca098e127c1efc370333f7774cfbce8bac0
interestor <tail6182@gmail.com>
2018-03-25 23:20:49 +0900

    フォームを追加した時の動きを足した。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,22 @@ class BooksController <  ApplicationController
   def new
     @book = Book.new
   end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      # @userはuser_path(@user) に自動変換される
+      redirect_to @book, notice: "本を登録しました。"
+    else
+      # ValidationエラーなどでDBに保存できない場合 new.html.erb を再表示
+      render 'new'
+    end
+  end
+
+  private
+  # Rails4からStrongParamaterと呼ばれる機能が追加されました。
+  # セキュリティのため、permitメソッドで許可したパラメータ名しか取得できません。
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
 end
