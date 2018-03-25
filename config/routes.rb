0a1c0c5f1ef89465e8f5846ef256f1acfec19a3b
interestor <tail6182@gmail.com>
2018-03-25 22:54:10 +0900

    bookを追加するためのコントローラーのnewメソッド、ビューを追加。

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/book_list", to: "book_list#show"
   get "/book_shelf", to: "book_shelf#show"
+  #get "/books/new", to: "books#new"
+  resources :books
 end
